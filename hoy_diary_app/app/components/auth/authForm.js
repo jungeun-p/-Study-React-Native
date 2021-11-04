27e24f20d131650f4543d681e66903875b220e83
jungeun-p <mrk7753@naver.com>
2021-11-04 14:34:47 +0900

    [ modify ] Form Type 상태값에 따른 컴포넌트 출력 처리
    - types라는 상태값 추가
    - updateInput : onChangeText 함수
    - conformPassword : Login 상태에 따른 비밀번호 재확인 input jsx 반환
    - formHasErrors : types.hasError 발생시 에러창 jsx 반환

--- hoy_diary_app/app/components/auth/authForm.js
+++ hoy_diary_app/app/components/auth/authForm.js
@@ -3,32 +3,102 @@ import {Text, View, TextInput, StyleSheet} from 'react-native';
 import InputCommon from '../../utils/forms/input';
 
 const AuthForm = () => {
-  const [myTextInput, setmyTestInput] = useState('');
+  const [types, setTypes] = useState({
+    type: 'Login',
+    action: 'Login',
+    actionMode: 'register',
+    hasErrors: true,
+    form: {
+      email: {
+        value: '',
+        type: 'textInput',
+        rules: {},
+      },
+      password: {
+        value: '',
+        type: 'textInput',
+        rules: {},
+        valid: false,
+      },
+      confirmPassword: {
+        value: '',
+        type: 'textInput',
+        rules: {},
+        valid: false,
+      },
+    },
+  });
+
+  const updateInput = (name, value) => {
+    setTypes({hasErrors: false});
+    let formCopy = types.form;
+    formCopy[name].value = value;
+    setTypes({form: formCopy});
+    console.warn(types.form);
+  };
+
+  // jsx 반환
+  const conformPassword = () =>
+    types.type !== 'Login' ? (
+      <InputCommon
+        value={types.form.confirmPassword.value}
+        type={types.form.confirmPassword.type}
+        secureTextEntry={true}
+        keyboardType={'email-address'}
+        placeholder={'password check'}
+        placeholderTextColor="#ddd"
+        onChangeText={value => updateInput('confirmPassword', value)}
+      />
+    ) : null;
+
+  // jsx 반환
+  const formHasErrors = () =>
+    types.hasErrors ? (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorLabel}>check the information of Login</Text>
+      </View>
+    ) : null;
+
   return (
     <View>
       <InputCommon
-        value={myTextInput}
+        value={types.form.email.value}
+        type={types.form.email.type}
         autoCapitalize={'none'}
         keyboardType={'email-address'}
         placeholder={'email address'}
         placeholderTextColor="#ddd"
+        onChangeText={value => updateInput('email', value)}
       />
       <InputCommon
-        value={myTextInput}
-        autoCapitalize={'none'}
+        value={types.form.password.value}
+        type={types.form.password.type}
+        secureTextEntry={true}
         keyboardType={'email-address'}
         placeholder={'password'}
         placeholderTextColor="#ddd"
+        onChangeText={value => updateInput('password', value)}
       />
-      <InputCommon
-        value={myTextInput}
-        autoCapitalize={'none'}
-        keyboardType={'email-address'}
-        placeholder={'password check'}
-        placeholderTextColor="#ddd"
-      />
+      {conformPassword()}
+      {formHasErrors()}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  errorContainer: {
+    marginBottom: 10,
+    marginTop: 30,
+    padding: 20,
+    backgroundColor: '#ee3344',
+  },
+  errorLabel: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlignVertical: 'cneter',
+    textAlign: 'center',
+  },
+});
 
 export default AuthForm;
--- hoy_diary_app/app/utils/forms/input.js
+++ hoy_diary_app/app/utils/forms/input.js
@@ -2,7 +2,18 @@ import React from 'react';
 import {TextInput, StyleSheet} from 'react-native';
 
 const InputCommon = props => {
-  return <TextInput {...props} style={styles.input} />;
+  let template = null;
+  switch (props.type) {
+    case 'textInput':
+      template = <TextInput {...props} style={styles.input} />;
+      break;
+    case 'textInputRevised':
+      template = <TextInput {...props} style={styles.inputRevised} />;
+      break;
+    default:
+      return template;
+  }
+  return template;
 };
 
 const styles = StyleSheet.create({
@@ -10,6 +21,15 @@ const styles = StyleSheet.create({
     width: '100%',
     borderBottomWidth: 1,
     borderBottomColor: '#fff',
+    fontSize: 13,
+    padding: 5,
+    marginTop: 15,
+    color: '#fff',
+  },
+  inputRevised: {
+    width: '100%',
+    borderBottomWidth: 3,
+    borderBottomColor: 'red',
     fontSize: 13,
     padding: 5,
     marginTop: 15,
