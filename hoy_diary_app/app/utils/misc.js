b0c2a4ec86e3375cb48f33050fb06132b0d2af10
jungeun-p <mrk7753@naver.com>
2021-11-21 14:44:24 +0900

    [ modify ] xcode error 설정, firebaseConfig 및 storage 설정
    - misc.js/ firebaeApp, storage, database
    - npm firebase/app

--- hoy_diary_app/app/utils/misc.js
+++ hoy_diary_app/app/utils/misc.js
@@ -1,9 +1,31 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {initializeApp} from 'firebase/app';
+import {getStorage} from 'firebase/storage';
+import {getDatabase} from 'firebase/database';
 
 export const APIKEY = 'AIzaSyBd9ijY9CmTNYee_YpoMYtqQRH7FOD6rVc';
 export const SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIKEY}`;
 export const SIGNIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${APIKEY}`;
 export const REFRESH = `https://securetoken.googleapis.com/v1/token?key=${APIKEY}`;
+
+// 사용되는 api 상세 내용
+const firebaseConfig = {
+  apiKey: 'AIzaSyBd9ijY9CmTNYee_YpoMYtqQRH7FOD6rVc',
+  authDomain: 'hoy-diary-app.firebaseapp.com',
+  databaseURL:
+    'https://hoy-diary-app-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'hoy-diary-app',
+  storageBucket: 'hoy-diary-app.appspot.com',
+  messagingSenderId: '792594232030',
+  appId: '1:792594232030:web:16e80695c19b89ffa3884f',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const storage = getStorage(firebaseApp);
+export const database = getDatabase(firebaseApp);
+
+// export const storage = firebase.storage();
+// export const database = firebase.database();
 
 export const setTokens = async (values, callBack) => {
   const firstPair = ['@hoydiary_app@userId', values.userId];
